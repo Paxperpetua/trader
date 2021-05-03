b80a7266c4c801213065e558953c16cb738c411c
Paxperpetua <paxperpetua@gmail.com>
2021-05-03 22:11:31 +0000

    styled devise forms and flash

---
+++ app/javascript/custom/custom.js
@@ -0,0 +1,6 @@
+import { show } from 'bootstrap';
+document.addEventListener("turbolinks:load", function() {
+  if ( true == true ) {
+    console.log('this conde is in custom.js.erb');
+  };
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  resources :customers, only: [ :index ]
+  devise_for :customers, controllers: {
+    registrations: 'customers/registrations'
+  }
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
