1ca22500e291481524062ae2903a286f540adc5c
Paxperpetua <paxperpetua@gmail.com>
2021-05-10 19:05:49 +0000

    fixed again2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+    IEX::Api.configure do |config|
+    config.publishable_token = ENV.fetch('IEX_API_PUBLISHABLE_TOKEN')
+    config.secret_token = ENV.fetch('IEX_API_SECRET_TOKEN')
+    config.endpoint = 'https://sandbox.iexapis.com/v1' # use 'https://sandbox.iexapis.com/v1' for Sandbox
+  end
+
 end
